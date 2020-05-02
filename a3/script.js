446945d34117b6da5169e17d1b3067c984700691
s3748430 <62051704+s3748430@users.noreply.github.com>
2020-05-02 08:42:17 +0700

    working on nav bar

--- a3/script.js
+++ a3/script.js
@@ -1,10 +1,17 @@
-<script
-  src="https://code.jquery.com/jquery-3.5.0.js"
-  integrity="sha256-r/AaFHrszJtwpe+tHyNi/XCfMxYpbsRg2Uqn0x3s2zc="
-  crossorigin="anonymous"></script>
-
-  function calculatePrice() {
-      alert("Calculate Price?!")
-  }
-  var 
-  script
+let navbar_height = document.getElementsById('navbar')[0].clientHeight
+console.log(document.getElementsById('navbar'))
+const aboutus_sec = document.getElementById('aboutus').offsetTop - 30
+const price_sec = document.getElementById('prices').offsetTop - 100
+const nowshowing_sec = document.getElementById('nowshowing').offsetTop
+const active_links = document.querySelectorAll('.nav-item')
+console.log(document.querySelectorAll('.nav-item'))
+function comparesectionFunction(){
+    var scroll_win = window.scrollY + navbar_height
+    if (scroll_win > aboutus_sec && price_sec > scroll_win)active_links[0].classList.add('active')
+    else active_links[0].classList.remove('active')
+    if (scroll_win > price_sec && nowshowing_sec>scroll_win)active_links[1].classList.add('active')
+    else active_links[1].classList.remove('active')
+    if (scroll_win> nowshowing_sec)active_links[2].classList.add('active')
+    else active_links[2].classList.remove('active')
+}
+document.addEventListener('scroll',comparesectionFunction)
